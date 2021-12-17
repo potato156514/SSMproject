bc6ff1a2c335dcc66b8c0a0dc14784e2d63f479c
potato156514 <1601848508@qq.com>
2021-12-17 15:44:56 +0800

    1.1.6

--- src/main/java/project/controller/webcontroller.java
+++ src/main/java/project/controller/webcontroller.java
@@ -1,6 +1,7 @@
 package project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +9,9 @@ public class webcontroller {
     @RequestMapping("/")
     public String hello(){
         return "login";
+    }
+    @GetMapping("/resetpassword")
+    public String resetpassword(){
+        return "resetpassword";
     }
 }
