e9c704fbd58fb6bf8d5ba96f4a324f513241bf6c
Boojux <1647284718@qq.com>
2022-01-04 11:35:48 +0800

    添加了错误页面

---
+++ src/main/java/project/Exception/SysExceptionResolver.java
@@ -0,0 +1,20 @@
+package project.Exception;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class SysExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("error","页面不存在！");
+        mv.setViewName("error");
+        return mv;
+    }
+}
